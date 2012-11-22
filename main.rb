f61d227bc1c7d449da3c553abaccf68baa2a9a95
Junta Hashimoto <euphur@gmail.com>
2012-11-22 09:56:05 +0900

    SDL works

---
+++ main.rb
@@ -0,0 +1,25 @@
+require 'sdl'
+
+
+class BlockPuzzle
+  def initialize
+    SDL.init(SDL::INIT_VIDEO)
+    @screen = SDL.set_video_mode(640, 480, 32, SDL::HWSURFACE)
+  end
+  def run
+    loop do
+      while event = SDL::Event.poll
+        process_event(event)
+      end
+    end
+  end
+  def process_event event
+    case event
+    when SDL::Event::Quit
+      exit
+    end
+  end
+end
+
+
+BlockPuzzle.new.run
